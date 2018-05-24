8456fea6507e33188ab42c1b9a86bc0eb81b24ec
NIVEDHAGOKUL <36990399+NIVEDHAGOKUL@users.noreply.github.com>
2018-05-24 14:51:28 +0530

    Create time minute.py

---
+++ time minute.py
@@ -0,0 +1,17 @@
+time = float(input("Input time in seconds: "))
+hour = time // 3600
+time %= 3600
+minutes = time // 60
+time %= 60
+print('hour:minutes')
+
+
+                                                                                                            d the sum of the first 40 terms of the arithmetic sequence 2,5,8,11,⋯ .
+                
+                                                                    First find the 40th term:
+
+                                                              a40=a1+(n−1)d        =2+39(3)=119
+
+                                                                Then find the sum:
+
+                                                      Sn=n(a1 + an)2S40=40(2 + 119)2=2420
